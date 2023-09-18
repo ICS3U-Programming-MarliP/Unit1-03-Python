2f2704780d0f0bd0cd66f6c5333f808f2be466c7
Marli Peters <marley.peters@stu.ocsb.ca>
2023-09-18 16:55:35 +0000

    :3

---
+++ hello_world_style.py
@@ -0,0 +1,14 @@
+#!usr/bin/env python3
+
+# Created By: Marli Peters
+# Date: Sep. 18, 2023
+# This program displays Hello world! with proper style.
+
+
+def main():
+    print("Hello world!")
+    print("I am testing very important stuff here!")
+
+
+if __name__ == "__main__":
+    main()
